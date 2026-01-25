4d718d20f30cfc5e1ac78cdf6074bb551e86a18b
Logan Woudstra <loganwoudstra@gmail.com>
2026-01-25 15:09:05 -0500

    sac implementation

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,5 +2,6 @@ from .MAPPO import MAPPO
 from .CentralizedMAPPO import CMAPPO
 from .QMIX import QMIX
 from .SARSA import SARSA
+from .SAC import SAC
 
-__all__ = ["MAPPO", "CMAPPO", "QMIX", "SARSA"]
+__all__ = ["MAPPO", "CMAPPO", "QMIX", "SARSA", 'SAC']
